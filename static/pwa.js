f95aa308c2694ffb792ef664c7b8f0a6b670687f
Tiago-beta <tgsantos66@hotmail.com>
2026-04-03 14:00:46 -0300

    style: adicionar contorno branco nos switches

--- static/pwa.js
+++ static/pwa.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/video/static/sw.js?v=20260403-5").catch(() => {
+    navigator.serviceWorker.register("/video/static/sw.js?v=20260403-6").catch(() => {
       // Ignore SW registration failures to avoid blocking the app.
     });
   });
