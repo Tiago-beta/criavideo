83a4220e10be4cae049526606fc31ca186619994
Tiago-beta <tgsantos66@hotmail.com>
2026-04-03 15:06:11 -0300

    UI: move script title below karaoke and refresh PWA cache

--- static/pwa.js
+++ static/pwa.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/video/static/sw.js?v=20260403-8").catch(() => {
+    navigator.serviceWorker.register("/video/static/sw.js?v=20260403-9").catch(() => {
       // Ignore SW registration failures to avoid blocking the app.
     });
   });
