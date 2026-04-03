8bdd77a3227c602d3e511f56fdd1ce15f96b84ce
Tiago-beta <tgsantos66@hotmail.com>
2026-04-03 13:11:54 -0300

    style: destacar botao Proximo com ouro 5% mais forte

--- static/pwa.js
+++ static/pwa.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/video/static/sw.js?v=20260403-3").catch(() => {
+    navigator.serviceWorker.register("/video/static/sw.js?v=20260403-4").catch(() => {
       // Ignore SW registration failures to avoid blocking the app.
     });
   });
