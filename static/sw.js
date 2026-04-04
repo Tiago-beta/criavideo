d4a38e459e7c81c2c29ab73d10f8298294c95ca7
Tiago-beta <tgsantos66@hotmail.com>
2026-04-04 15:00:55 -0300

    Icone do app com fundo branco e favicon transparente

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "criavideo-shell-v39";
+const CACHE_NAME = "criavideo-shell-v40";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
   "/video/static/style.css?v=20260404-05",
   "/video/static/app.js?v=20260404-05",
-  "/video/static/pwa.js?v=20260404-05",
+  "/video/static/pwa.js?v=20260404-06",
   "/video/static/icons/login-logo.png?v=20260404-05",
-  "/video/static/icons/icon-192.png?v=20260404-05",
-  "/video/static/icons/icon-512.png?v=20260404-05",
+  "/video/static/icons/icon-192.png?v=20260404-06",
+  "/video/static/icons/icon-512.png?v=20260404-06",
 ];
 
 // HTML pages that should use network-first strategy
