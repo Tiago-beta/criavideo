8a6419910e0de7848fa58ea073c3abf8db8fc685
Tiago-beta <tgsantos66@hotmail.com>
2026-04-04 17:09:00 -0300

    Aumentar icone mobile com maskable e zoom agressivo

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "criavideo-shell-v47";
+const CACHE_NAME = "criavideo-shell-v48";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
-  "/video/static/style.css?v=20260404-13",
-  "/video/static/app.js?v=20260404-13",
-  "/video/static/pwa.js?v=20260404-13",
-  "/video/static/icons/login-logo.png?v=20260404-13",
-  "/video/static/icons/icon-192.png?v=20260404-13",
-  "/video/static/icons/icon-512.png?v=20260404-13",
+  "/video/static/style.css?v=20260404-14",
+  "/video/static/app.js?v=20260404-14",
+  "/video/static/pwa.js?v=20260404-14",
+  "/video/static/icons/login-logo.png?v=20260404-14",
+  "/video/static/icons/icon-192.png?v=20260404-14",
+  "/video/static/icons/icon-512.png?v=20260404-14",
 ];
 
 // HTML pages that should use network-first strategy
