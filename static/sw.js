143989137e64d9c1efdfaf85d00c0755f5205ed4
Tiago-beta <tgsantos66@hotmail.com>
2026-04-04 15:06:19 -0300

    Atualiza logo do login para versao com texto CriaVideo

--- static/sw.js
+++ static/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "criavideo-shell-v40";
+const CACHE_NAME = "criavideo-shell-v41";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
   "/video/static/style.css?v=20260404-05",
   "/video/static/app.js?v=20260404-05",
-  "/video/static/pwa.js?v=20260404-06",
-  "/video/static/icons/login-logo.png?v=20260404-05",
+  "/video/static/pwa.js?v=20260404-07",
+  "/video/static/icons/login-logo.png?v=20260404-07",
   "/video/static/icons/icon-192.png?v=20260404-06",
   "/video/static/icons/icon-512.png?v=20260404-06",
 ];
