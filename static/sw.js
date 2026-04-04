3ef3b99dc6b26adb76d4d36f8f35fc06acdcee9e
Tiago-beta <tgsantos66@hotmail.com>
2026-04-04 11:47:48 -0300

    Favicon transparente, logo no sidebar, remove textos login

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "criavideo-shell-v37";
+const CACHE_NAME = "criavideo-shell-v38";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
-  "/video/static/style.css?v=20260404-04",
+  "/video/static/style.css?v=20260404-05",
   "/video/static/app.js?v=20260404-03",
-  "/video/static/pwa.js?v=20260404-04",
-  "/video/static/icons/login-logo.png?v=20260404-04",
-  "/video/static/icons/icon-192.png?v=20260404-03",
-  "/video/static/icons/icon-512.png?v=20260404-03",
+  "/video/static/pwa.js?v=20260404-05",
+  "/video/static/icons/login-logo.png?v=20260404-05",
+  "/video/static/icons/icon-192.png?v=20260404-05",
+  "/video/static/icons/icon-512.png?v=20260404-05",
 ];
 
 self.addEventListener("install", (event) => {
