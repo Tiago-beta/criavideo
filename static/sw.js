2745369121860c96a2619c10e8a01578cc146f2f
Tiago-beta <tgsantos66@hotmail.com>
2026-04-12 10:32:40 -0300

    fix: bumpar sw.js cache v59 para forcar atualizacao no cliente

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "criavideo-shell-v58";
+const CACHE_NAME = "criavideo-shell-v59";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
-  "/video/static/style.css?v=20260410-01",
-  "/video/static/app.js?v=20260410-01",
-  "/video/static/pwa.js?v=20260410-01",
+  "/video/static/style.css?v=20260412-03",
+  "/video/static/app.js?v=20260412-05",
+  "/video/static/pwa.js?v=20260412-01",
   "/video/static/icons/login-logo.png?v=20260410-01",
   "/video/static/icons/icon-192.png?v=20260410-01",
   "/video/static/icons/icon-512.png?v=20260410-01",
