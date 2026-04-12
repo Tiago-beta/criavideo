88ef3ae0917291e953c91bfc1d2df4a106e98120
Tiago-beta <tgsantos66@hotmail.com>
2026-04-12 10:51:45 -0300

    fix: restaurar arquivos perdidos por formatter externo + bumpar SW v61

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "criavideo-shell-v60";
+const CACHE_NAME = "criavideo-shell-v61";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
-  "/video/static/style.css?v=20260412-03",
-  "/video/static/app.js?v=20260412-05",
-  "/video/static/pwa.js?v=20260412-05",
+  "/video/static/style.css?v=20260412-06",
+  "/video/static/app.js?v=20260412-06",
+  "/video/static/pwa.js?v=20260412-06",
   "/video/static/icons/login-logo.png?v=20260411-02",
   "/video/static/icons/icon-192.png?v=20260411-02",
   "/video/static/icons/icon-512.png?v=20260411-02",
