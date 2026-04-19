9525c069f4d98d6873cbe51fb14179134d2156ec
Tiago-beta <tgsantos66@hotmail.com>
2026-04-19 17:00:31 -0300

    Editor: remover botao Cortar do menu superior

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "criavideo-shell-v165";
+const CACHE_NAME = "criavideo-shell-v166";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
-  "/video/static/style.css?v=20260419-46",
-  "/video/static/app.js?v=20260419-46",
-  "/video/static/pwa.js?v=20260419-46",
+  "/video/static/style.css?v=20260419-47",
+  "/video/static/app.js?v=20260419-47",
+  "/video/static/pwa.js?v=20260419-47",
   "/video/static/icons/login-logo.png?v=20260411-02",
   "/video/static/icons/icon-192.png?v=20260411-02",
   "/video/static/icons/icon-512.png?v=20260411-02",
