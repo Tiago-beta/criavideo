cc4a182c652886f4b75a393016effb6985b18658
Tiago-beta <tgsantos66@hotmail.com>
2026-04-21 14:11:47 -0300

    Limpa textos auxiliares e simplifica botoes

--- static/pwa.js
+++ static/pwa.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/video/static/sw.js?v=20260421-07").catch(() => {
+    navigator.serviceWorker.register("/video/static/sw.js?v=20260421-08").catch(() => {
       // Ignore SW registration failures to avoid blocking the app.
     });
   });
