1b4bfe73721d25e56a6b7c1e4d685b3ee0987fc4
Tiago-beta <tgsantos66@hotmail.com>
2026-05-01 17:02:15 -0300

    Corrige hotfix da UI semelhante

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "criavideo-shell-v318";
+const CACHE_NAME = "criavideo-shell-v321";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
-  "/video/static/style.css?v=20260501-02",
-  "/video/static/app.js?v=20260501-02",
-  "/video/static/pwa.js?v=20260501-02",
+  "/video/static/style.css?v=20260501-04",
+  "/video/static/app.js?v=20260501-04",
+  "/video/static/pwa.js?v=20260501-04",
   "/video/static/icons/login-logo.png?v=20260411-02",
   "/video/static/icons/icon-192.png?v=20260411-02",
   "/video/static/icons/icon-512.png?v=20260411-02",
