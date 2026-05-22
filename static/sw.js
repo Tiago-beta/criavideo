d0341ff9113ee78d194bf9555258446f91cc4af5
Tiago-beta <tgsantos66@hotmail.com>
2026-05-21 22:23:10 -0300

    Fix editor timeline dead-zone collapse

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "criavideo-shell-v520";
+const CACHE_NAME = "criavideo-shell-v521";
 const ASSETS = [
   "/video",
   "/video/static/index.html",
-  "/video/static/style.css?v=20260521-08",
-  "/video/static/app.js?v=20260521-08",
-  "/video/static/pwa.js?v=20260521-08",
+  "/video/static/style.css?v=20260521-09",
+  "/video/static/app.js?v=20260521-09",
+  "/video/static/pwa.js?v=20260521-09",
   "/video/static/admin-panel.html?v=20260516-10",
   "/video/static/icons/login-logo.png?v=20260411-02",
   "/video/static/icons/icon-192.png?v=20260411-02",
