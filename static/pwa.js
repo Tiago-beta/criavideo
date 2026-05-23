0b9bae62fbc077f0bd899a6093193d7e9868b4fc
Tiago-beta <tgsantos66@hotmail.com>
2026-05-23 17:36:33 -0300

    Fix create modal audio builder layout

--- static/pwa.js
+++ static/pwa.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/video/static/sw.js?v=20260523-01").catch(() => {
+    navigator.serviceWorker.register("/video/static/sw.js?v=20260523-02").catch(() => {
       // Ignore SW registration failures to avoid blocking the app.
     });
   });
